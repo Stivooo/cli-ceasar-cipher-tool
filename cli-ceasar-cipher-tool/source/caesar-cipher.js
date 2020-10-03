d02672c1a4ab4d9815d7d19056c6c09be0f91ae2
Владимир Рыбин <vrybin@livemaster.ru>
2020-10-03 21:52:15 +0300

    feat: add caear-cipher algorithm

---
+++ cli-ceasar-cipher-tool/source/caesar-cipher.js
@@ -0,0 +1,44 @@
+const UPPERCASE_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const LOWERCASE_ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+const OPERATION_TYPE_ENCODE = 'encode'
+const OPERATION_TYPE_DECODE = 'decode'
+
+const shiftSymbol = (symbol, shift, alphabet) => {
+	let index = alphabet.indexOf(symbol)
+	let shiftedIndex = index + shift
+	if (shiftedIndex > alphabet.length) {
+		shiftedIndex = shiftedIndex % alphabet.length
+	}
+	return alphabet[shiftedIndex]
+}
+
+const unshiftSymbol = (symbol, shift, alphabet) => {
+	let index = alphabet.indexOf(symbol)
+	let shiftedIndex = index - shift + alphabet.length
+	if (shiftedIndex > alphabet.length) {
+		shiftedIndex = shiftedIndex % alphabet.length
+	}
+	return alphabet[shiftedIndex]
+}
+
+const makeCeasarCipher = (string, shift, type) => {
+	if (!string.length) throw Error('The entered string is empty')
+	if (shift < 0) throw Error('Value of shift must be positive')
+
+	const symbolsList = string.split('')
+	const outSymbols = symbolsList.map(symbol => {
+		if (symbol.match(/^[A-Za-z]/) === null) return symbol
+		const targetAlphabet = symbol.match(/^[a-z]/) ? LOWERCASE_ALPHABET : UPPERCASE_ALPHABET
+		switch (type) {
+			case OPERATION_TYPE_ENCODE:
+				return shiftSymbol(symbol, shift, targetAlphabet)
+			case OPERATION_TYPE_DECODE:
+				return unshiftSymbol(symbol, shift, targetAlphabet)
+			default:
+				throw Error('Unknown operation type')
+		}
+	})
+	return outSymbols.join('')
+}
+
+module.exports = makeCeasarCipher
